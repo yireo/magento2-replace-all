962a0a1cb3d22d968194ef4cf0ce762c44c65031
Jisse Reitsma <jisse@yireo.com>
2019-07-08 16:35:01 +0200

    Reactivate single tests

--- magento2-run-tests.sh
+++ magento2-run-tests.sh
@@ -33,10 +33,10 @@ function removePackages() {
     composer remove --no-update $packages
 }
 
-#addPackages $core; reconfigureMagento(); removePackages $core
-#addPackages $bundled; reconfigureMagento(); removePackages $bundled
-#addPackages $graphql; reconfigureMagento(); removePackages $graphql
-#addPackages $inventory; reconfigureMagento(); removePackages $inventory
+addPackages $core; reconfigureMagento(); removePackages $core
+addPackages $bundled; reconfigureMagento(); removePackages $bundled
+addPackages $graphql; reconfigureMagento(); removePackages $graphql
+addPackages $inventory; reconfigureMagento(); removePackages $inventory
 
 addPackages $core $bundled; reconfigureMagento; removePackages $core $bundled
 addPackages $core $graphql; reconfigureMagento; removePackages $core $graphql
